28c7369a4ea89d641faa89b059e6b9f57c5f3222
bryanseely <bryan@bryanseely.me>
2025-12-13 18:57:56 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: This must match your GitHub repository name exactly, with slashes.
+  base: "/diamond-days/", 
 })
